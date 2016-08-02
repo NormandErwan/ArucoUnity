d4b7d4857c1a0de131869490f0dba38b32cab828
Erwan Normand <normand.erwan@gmail.com>
2016-08-02 18:12:03 -0400

    feat(aruco_unity): created a basic plugin for testing with Unity

---
+++ include/aruco_unity.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <aruco/aruco.h>
+#include <aruco/cvdrawingutils.h>
+#include <opencv2/highgui/highgui.hpp>
+
+#ifdef WIN32
+  #ifdef aruco_unity_EXPORTS
+    #define ARUCO_UNITY_API __declspec(dllexport)
+  #else
+    #define ARUCO_UNITY_API __declspec(dllimport)
+  #endif
+#else
+  #define ARUCO_UNITY_API
+#endif
+
+extern "C" {
+  ARUCO_UNITY_API int testMult(int a, int b);
+  ARUCO_UNITY_API void detectMarkers(char* filename);
+}
---
+++ src/aruco_unity.cpp
@@ -0,0 +1,39 @@
+#include "../include/aruco_unity.hpp"
+
+extern "C" {
+
+int testMult(int a, int b) 
+{
+  return a * b;
+}
+
+void detectMarkers(char* filename)
+{
+  try
+  {
+    aruco::MarkerDetector MDetector;
+    std::vector<aruco::Marker> Markers;
+
+    //read the input image
+    cv::Mat InImage;
+    InImage = cv::imread(filename);
+
+    //Ok, let's detect
+    MDetector.detect(InImage,Markers);
+
+    //for each marker, draw info and its boundaries in the image
+    for (unsigned int i = 0; i < Markers.size(); i++) {
+      std::cout << Markers[i] << std::endl;
+      Markers[i].draw(InImage, cv::Scalar(0,0,255), 2);
+    }
+    
+    cv::imshow("in", InImage);
+    cv::waitKey(0); //wait for key to be pressed
+  } 
+  catch (std::exception &exception)
+  {
+    std::cout << "Exception: " << exception.what() << std::endl;
+  }
+}
+
+}
