6ede18f770de350670aa0097469536f20ab8552e
Erwan Normand <normand.erwan@gmail.com>
2016-09-28 17:45:26 -0400

    doc(dictionary.hpp): update the documentation

--- docs/search/defines_0.js
+++ docs/search/defines_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['aruco_5funity_5fapi',['ARUCO_UNITY_API',['../exports_8hpp.html#a8d3ccc1741420a252882c86d41c10276',1,'exports.hpp']]],
-  ['aruco_5funity_5ftry_5fcatch',['ARUCO_UNITY_TRY_CATCH',['../exception_8hpp.html#a949de7da977f6853cd77a1003d27dcdc',1,'exception.hpp']]]
+  ['aruco_5funity_5fcopy_5fexception',['ARUCO_UNITY_COPY_EXCEPTION',['../exception_8hpp.html#a0a830cdae02bb7c5c0df9b2517fa2e63',1,'exception.hpp']]]
 ];
