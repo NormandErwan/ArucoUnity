4e94c379ae486dac97a5757b47f91ed7cd2ff188
Erwan Normand <normand.erwan@gmail.com>
2016-10-17 15:25:52 -0400

    feat(vector_vec3d.hpp, VectorVec3d.cs, vector_vec4i.hpp, VectorVec4i.cs): added classes

---
+++ src/aruco_unity_package/Assets/ArucoUnity/Scripts/Utility/VectorVec3d.cs
@@ -0,0 +1,76 @@
+using System.Runtime.InteropServices;
+
+namespace ArucoUnity
+{
+  namespace Utility
+  {
+    public class VectorVec3d : HandleCvPtr
+    {
+      // Constructor & Destructor
+      [DllImport("ArucoUnity")]
+      static extern System.IntPtr au_vectorVec3d_new();
+
+      [DllImport("ArucoUnity")]
+      static extern void au_vectorVec3d_delete(System.IntPtr vector);
+
+      // Functions
+      [DllImport("ArucoUnity")]
+      static extern System.IntPtr au_vectorVec3d_at(System.IntPtr vector, uint pos, System.IntPtr exception);
+
+      [DllImport("ArucoUnity")]
+      static extern unsafe System.IntPtr* au_vectorVec3d_data(System.IntPtr vector);
+
+      [DllImport("ArucoUnity")]
+      static extern void au_vectorVec3d_push_back(System.IntPtr vector, System.IntPtr value);
+
+      [DllImport("ArucoUnity")]
+      static extern uint au_vectorVec3d_size(System.IntPtr vector);
+
+      public VectorVec3d() : base(au_vectorVec3d_new())
+      {
+      }
+
+      public VectorVec3d(System.IntPtr vectorVec3dPtr, DeleteResponsibility deleteResponsibility = DeleteResponsibility.True) 
+        : base(vectorVec3dPtr, deleteResponsibility)
+      {
+      }
+
+      protected override void DeleteCvPtr()
+      {
+        au_vectorVec3d_delete(cvPtr);
+      }
+
+      public Vec3d At(uint pos) 
+      {
+        Exception exception = new Exception();
+        Vec3d element = new Vec3d(au_vectorVec3d_at(cvPtr, pos, exception.cvPtr), DeleteResponsibility.False);
+        exception.Check();
+        return element;
+      }
+
+      public unsafe Vec3d[] Data()
+      {
+        System.IntPtr* dataPtr = au_vectorVec3d_data(cvPtr);
+        uint size = Size();
+
+        Vec3d[] data = new Vec3d[size];
+        for (int i = 0; i < size; i++)
+        {
+          data[i] = new Vec3d(dataPtr[i], DeleteResponsibility.False);
+        }
+
+        return data;
+      }
+
+      public void PushBack(Vec3d value)
+      {
+        au_vectorVec3d_push_back(cvPtr, value.cvPtr);
+      }
+
+      public uint Size()
+      {
+        return au_vectorVec3d_size(cvPtr);
+      }
+    }
+  }
+}
---
+++ src/aruco_unity_package/Assets/ArucoUnity/Scripts/Utility/VectorVec4i.cs
@@ -0,0 +1,76 @@
+using System.Runtime.InteropServices;
+
+namespace ArucoUnity
+{
+  namespace Utility
+  {
+    public class VectorVec4i : HandleCvPtr
+    {
+      // Constructor & Destructor
+      [DllImport("ArucoUnity")]
+      static extern System.IntPtr au_vectorVec4i_new();
+
+      [DllImport("ArucoUnity")]
+      static extern void au_vectorVec4i_delete(System.IntPtr vector);
+
+      // Functions
+      [DllImport("ArucoUnity")]
+      static extern System.IntPtr au_vectorVec4i_at(System.IntPtr vector, uint pos, System.IntPtr exception);
+
+      [DllImport("ArucoUnity")]
+      static extern unsafe System.IntPtr* au_vectorVec4i_data(System.IntPtr vector);
+
+      [DllImport("ArucoUnity")]
+      static extern void au_vectorVec4i_push_back(System.IntPtr vector, System.IntPtr value);
+
+      [DllImport("ArucoUnity")]
+      static extern uint au_vectorVec4i_size(System.IntPtr vector);
+
+      public VectorVec4i() : base(au_vectorVec4i_new())
+      {
+      }
+
+      public VectorVec4i(System.IntPtr vectorVec4iPtr, DeleteResponsibility deleteResponsibility = DeleteResponsibility.True) 
+        : base(vectorVec4iPtr, deleteResponsibility)
+      {
+      }
+
+      protected override void DeleteCvPtr()
+      {
+        au_vectorVec4i_delete(cvPtr);
+      }
+
+      public Vec4i At(uint pos) 
+      {
+        Exception exception = new Exception();
+        Vec4i element = new Vec4i(au_vectorVec4i_at(cvPtr, pos, exception.cvPtr), DeleteResponsibility.False);
+        exception.Check();
+        return element;
+      }
+
+      public unsafe Vec4i[] Data()
+      {
+        System.IntPtr* dataPtr = au_vectorVec4i_data(cvPtr);
+        uint size = Size();
+
+        Vec4i[] data = new Vec4i[size];
+        for (int i = 0; i < size; i++)
+        {
+          data[i] = new Vec4i(dataPtr[i], DeleteResponsibility.False);
+        }
+
+        return data;
+      }
+
+      public void PushBack(Vec4i value)
+      {
+        au_vectorVec4i_push_back(cvPtr, value.cvPtr);
+      }
+
+      public uint Size()
+      {
+        return au_vectorVec4i_size(cvPtr);
+      }
+    }
+  }
+}
