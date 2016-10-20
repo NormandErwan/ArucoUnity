73423dff53bce10422a9fbf52a1058f52e26fe53
Erwan Normand <normand.erwan@gmail.com>
2016-10-20 14:59:01 -0400

    feat(CalibrateCamera.cs, CameraParameters.cs): completed calibration prefab and save the camera parameters on a xml file.

--- src/aruco_unity_package/Assets/ArucoUnity/Scripts/Prefabs/CalibrateCamera.cs
+++ src/aruco_unity_package/Assets/ArucoUnity/Scripts/Prefabs/CalibrateCamera.cs
@@ -8,20 +8,6 @@ namespace ArucoUnity
   {
     public class CalibrateCamera : MonoBehaviour
     {
-      public Dictionary dictionary;
-      public DetectorParameters detectorParameters;
-      public GridBoard board;
-
-      [HideInInspector]
-      public Texture2D imageTexture;
-
-      [HideInInspector]
-      public CALIB calibrationFlags;
-
-      public Utility.VectorVectorVectorPoint2f allCorners;
-      public Utility.VectorVectorInt allIds;
-      public Utility.Size imageSize;
-
       [Header("Board configuration")]
       [SerializeField]
       [Tooltip("Number of markers in X direction")]
@@ -47,16 +33,19 @@ namespace ArucoUnity
       private DetectorParametersManager detectorParametersManager;
 
       [SerializeField]
-      public bool applyRefindStrategy = false;
+      private bool applyRefindStrategy = false;
 
       [SerializeField]
-      public bool assumeZeroTangentialDistorsion = false;
+      private bool assumeZeroTangentialDistorsion = false;
 
       [SerializeField]
-      public float fixAspectRatio;
+      private float fixAspectRatio;
 
       [SerializeField]
-      public bool fixPrincipalPointAtCenter = false;
+      private bool fixPrincipalPointAtCenter = false;
+
+      [SerializeField]
+      private string outputFilePath;
 
       [Header("Camera configuration")]
       [SerializeField]
@@ -64,14 +53,35 @@ namespace ArucoUnity
 
       [Header("UI")]
       [SerializeField]
-      private Button AddFrameButton;
+      private Button addFrameButton;
 
       [SerializeField]
-      private Button CalibrateButton;
+      private Button calibrateButton;
 
       [SerializeField]
-      private Text ImagesForCalibrationText;
+      private Text imagesForCalibrationText;
 
+      [SerializeField]
+      private Text calibrationReprojectionError;
+
+      public GridBoard Board { get; set; }
+      public Dictionary Dictionary { get; set; }
+      public DetectorParameters DetectorParameters { get; set; }
+
+      public bool ApplyRefindStrategy { get { return applyRefindStrategy; } set { applyRefindStrategy = value; } }
+      public bool AssumeZeroTangentialDistorsion { get { return assumeZeroTangentialDistorsion; } set { assumeZeroTangentialDistorsion = value; } }
+      public float FixAspectRatio { get { return fixAspectRatio; } set { fixAspectRatio = value; } }
+      public bool FixPrincipalPointAtCenter { get { return fixPrincipalPointAtCenter; } set { fixPrincipalPointAtCenter = value; } }
+      public CALIB CalibrationFlags { get; set; }
+      public string OutputFilePath { get { return outputFilePath; } set { outputFilePath = value; } }
+
+      public Utility.VectorVectorVectorPoint2f AllCorners { get; set; }
+      public Utility.VectorVectorInt AllIds { get; set; }
+      public Utility.Size ImageSize { get; set; }
+      public Texture2D ImageTexture { get; set; }
+      public double CalibrationReprojectionError { get; set; }
+
+      private CameraParameters cameraParameters;
       private bool addNextFrame;
       private bool calibrate;
 
@@ -89,25 +99,20 @@ namespace ArucoUnity
       {
         if (deviceCameraController.cameraStarted)
         {
-          // Detect and draw markers
           Utility.VectorVectorPoint2f corners;
           Utility.VectorInt ids;
           Utility.VectorVectorPoint2f rejectedImgPoints;
           Utility.Mat image;
 
-          imageTexture.SetPixels32(deviceCameraController.activeCameraTexture.GetPixels32());
+          // Detect and draw markers
+          ImageTexture.SetPixels32(deviceCameraController.activeCameraTexture.GetPixels32());
           Detect(out corners, out ids, out rejectedImgPoints, out image);
 
           // Add frame to calibration frame list
-          if (addNextFrame && !calibrate)
+          if (!calibrate)
           {
-            addNextFrame = false;
-            CalibrateButton.enabled = true;
-
-            allCorners.PushBack(corners);
-            allIds.PushBack(ids);
-            imageSize = image.size;
-            
+            AddFrameForCalibration(corners, ids, image);
+            calibrateButton.enabled = true;
             UpdateImagesForCalibrationText();
           }
         }
@@ -115,81 +120,68 @@ namespace ArucoUnity
 
       void Configurate()
       {
-        dictionary = Methods.GetPredefinedDictionary(dictionaryName);
-        detectorParameters = detectorParametersManager.detectorParameters;
-        board = GridBoard.Create(markersNumberX, markersNumberY, markerLength, markerSeparation, dictionary);
+        Dictionary = Methods.GetPredefinedDictionary(dictionaryName);
+        DetectorParameters = detectorParametersManager.detectorParameters;
+        Board = GridBoard.Create(markersNumberX, markersNumberY, markerLength, markerSeparation, Dictionary);
 
         ConfigurateCalibrationFlags();
         ConfigurateImageTexture(deviceCameraController);
         ResetCalibration();
       }
 
-      // Call it first if you're using the Script alone, not with the Prefab.
-      void Configurate(Dictionary dictionary, DetectorParameters detectorParameters, GridBoard board, Texture2D imageTexture, bool applyRefindStrategy, 
-        bool assumeZeroTangentialDistorsion, float fixAspectRatio, bool fixPrincipalPointAtCenter)
+      public void ConfigurateCalibrationFlags()
       {
-        this.dictionary = dictionary;
-        this.detectorParameters = detectorParameters;
-        this.board = board;
-
-        this.imageTexture = imageTexture;
-        this.applyRefindStrategy = applyRefindStrategy;
-        this.assumeZeroTangentialDistorsion = assumeZeroTangentialDistorsion;
-        this.fixAspectRatio = fixAspectRatio;
-        this.fixPrincipalPointAtCenter = fixPrincipalPointAtCenter;
-
-        ConfigurateCalibrationFlags();
-        ResetCalibration();
-      }
-
-      void ConfigurateCalibrationFlags()
-      {
-        calibrationFlags = 0;
+        CalibrationFlags = 0;
         if (assumeZeroTangentialDistorsion)
         {
-          calibrationFlags |= CALIB.ZERO_TANGENT_DIST;
+          CalibrationFlags |= CALIB.ZERO_TANGENT_DIST;
         }
         if (fixAspectRatio > 0)
         {
-          calibrationFlags |= CALIB.FIX_ASPECT_RATIO;
+          CalibrationFlags |= CALIB.FIX_ASPECT_RATIO;
         }
         if (fixPrincipalPointAtCenter)
         {
-          calibrationFlags |= CALIB.FIX_PRINCIPAL_POINT;
+          CalibrationFlags |= CALIB.FIX_PRINCIPAL_POINT;
         }
       }
 
       public void ConfigurateImageTexture(DeviceCameraController deviceCameraController)
       {
-        imageTexture = new Texture2D(deviceCameraController.activeCameraTexture.width, deviceCameraController.activeCameraTexture.height,
+        ImageTexture = new Texture2D(deviceCameraController.activeCameraTexture.width, deviceCameraController.activeCameraTexture.height,
           TextureFormat.RGB24, false);
-        deviceCameraController.SetActiveTexture(imageTexture);
+        deviceCameraController.SetActiveTexture(ImageTexture);
       }
 
       public void ResetCalibration()
       {
         calibrate = false;
-        AddFrameButton.enabled = true;
-        CalibrateButton.enabled = false;
-
-        allCorners = new Utility.VectorVectorVectorPoint2f();
-        allIds = new Utility.VectorVectorInt();
-        imageSize = new Utility.Size();
-
-        UpdateImagesForCalibrationText();
+        AllCorners = new Utility.VectorVectorVectorPoint2f();
+        AllIds = new Utility.VectorVectorInt();
+        ImageSize = new Utility.Size();
       }
 
-      public void Detect(out Utility.VectorVectorPoint2f corners, out Utility.VectorInt ids, 
-        out Utility.VectorVectorPoint2f rejectedImgPoints, out Utility.Mat image)
+      public void ResetCalibrationFromEditor()
+      {
+        addFrameButton.enabled = true;
+        calibrateButton.enabled = false;
+
+        ResetCalibration();
+        UpdateImagesForCalibrationText();
+        calibrationReprojectionError.text = "Calibration reprojection error: 0";
+      }
+
+      public void Detect(out Utility.VectorVectorPoint2f corners, out Utility.VectorInt ids, out Utility.VectorVectorPoint2f rejectedImgPoints, 
+        out Utility.Mat image)
       {
         // Detect markers
-        byte[] imageData = imageTexture.GetRawTextureData();
-        image = new Utility.Mat(imageTexture.height, imageTexture.width, imageData);
-        Methods.DetectMarkers(image, dictionary, out corners, out ids, detectorParameters, out rejectedImgPoints);
+        byte[] imageData = ImageTexture.GetRawTextureData();
+        image = new Utility.Mat(ImageTexture.height, ImageTexture.width, TYPE.CV_8UC3, imageData);
+        Methods.DetectMarkers(image, Dictionary, out corners, out ids, DetectorParameters, out rejectedImgPoints);
 
         if (applyRefindStrategy)
         {
-          //Methods.RefineDetectedMarkers(image, board, corners, ids, rejectedImgPoints); // TODO: fix the method
+          Methods.RefineDetectedMarkers(image, Board, corners, ids, rejectedImgPoints);
         }
 
         // Draw results
@@ -199,8 +191,8 @@ namespace ArucoUnity
         }
 
         int imageDataSize = (int)(image.ElemSize() * image.Total());
-        imageTexture.LoadRawTextureData(image.data, imageDataSize);
-        imageTexture.Apply(false);
+        ImageTexture.LoadRawTextureData(image.data, imageDataSize);
+        ImageTexture.Apply(false);
       }
 
       public void AddNextFrameForCalibration()
@@ -208,47 +200,87 @@ namespace ArucoUnity
         addNextFrame = true;
       }
 
-      public double Calibrate()
+      public void AddFrameForCalibration(Utility.VectorVectorPoint2f corners, Utility.VectorInt ids, Utility.Mat image)
+      {
+        if (addNextFrame && !calibrate)
+        {
+          if (ids.Size() < 1)
+          {
+            Debug.LogError("Not enough markers detected to add the frame for calibration.");
+            return;
+          }
+
+          addNextFrame = false;
+
+          AllCorners.PushBack(corners);
+          AllIds.PushBack(ids);
+          ImageSize = image.size;
+        }
+      }
+
+      public void Calibrate(string calibrationFilePath)
       {
         calibrate = true;
-        AddFrameButton.enabled = false;
-        CalibrateButton.enabled = false;
+
+        Utility.Mat cameraMatrix;
+        Utility.Mat distCoeffs = new Utility.Mat();
+        Utility.VectorMat rvecs, tvecs;
+
+        if ((CalibrationFlags & CALIB.FIX_ASPECT_RATIO) == CALIB.FIX_ASPECT_RATIO)
+        {
+          cameraMatrix = new Utility.Mat(3, 3, TYPE.CV_64F, new double[9] { fixAspectRatio, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0 });
+        }
+        else
+        {
+          cameraMatrix = new Utility.Mat();
+        }
 
         // Prepare data for calibration
         Utility.VectorVectorPoint2f allCornersContenated = new Utility.VectorVectorPoint2f();
         Utility.VectorInt allIdsContanated = new Utility.VectorInt();
         Utility.VectorInt markerCounterPerFrame = new Utility.VectorInt();
 
-        uint allCornersSize = allCorners.Size();
+        uint allCornersSize = AllCorners.Size();
         markerCounterPerFrame.Reserve(allCornersSize);
         for (uint i = 0; i < allCornersSize; i++)
         {
-          Utility.VectorVectorPoint2f allCornersI = allCorners.At(i);
+          Utility.VectorVectorPoint2f allCornersI = AllCorners.At(i);
           uint allCornersISize = allCornersI.Size();
           markerCounterPerFrame.PushBack((int)allCornersISize);
           for (uint j = 0; j < allCornersISize; j++)
           {
             allCornersContenated.PushBack(allCornersI.At(j));
-            allIdsContanated.PushBack(allIds.At(i).At(j));
+            allIdsContanated.PushBack(AllIds.At(i).At(j));
           }
         }
 
         // Calibrate camera
-        Utility.Mat cameraMatrix = new Utility.Mat();
-        Utility.Mat distCoeffs = new Utility.Mat();
-        Utility.VectorMat rvecs, tvecs;
-        double repError = Methods.CalibrateCameraAruco(allCornersContenated, allIdsContanated, markerCounterPerFrame, board, imageSize, cameraMatrix, 
-          distCoeffs, out rvecs, out tvecs, (int)calibrationFlags);
+        CalibrationReprojectionError = Methods.CalibrateCameraAruco(allCornersContenated, allIdsContanated, markerCounterPerFrame, Board, ImageSize, 
+          cameraMatrix, distCoeffs, out rvecs, out tvecs, (int)CalibrationFlags);
 
         // Save camera parameters
-        // TODO
+        cameraParameters = new CameraParameters(cameraMatrix, distCoeffs)
+        {
+          ImageHeight = ImageSize.height,
+          ImageWidth = ImageSize.width,
+          CalibrationFlags = (int)CalibrationFlags,
+          AspectRatio = fixAspectRatio,
+          ReprojectionError = CalibrationReprojectionError
+        };
+        cameraParameters.SaveToXmlFile(calibrationFilePath);
+      }
 
-        return repError;
+      public void CalibrateFromEditor()
+      {
+        addFrameButton.enabled = false;
+        calibrateButton.enabled = false;
+        Calibrate(outputFilePath);
+        calibrationReprojectionError.text = "Calibration reprojection error: " + CalibrationReprojectionError.ToString("F3");
       }
 
       void UpdateImagesForCalibrationText()
       {
-        ImagesForCalibrationText.text = "Images for calibration: " + allIds.Size();
+        imagesForCalibrationText.text = "Images for calibration: " + AllIds.Size();
       }
     }
   }
---
+++ src/aruco_unity_package/Assets/ArucoUnity/Scripts/Prefabs/CameraParameters.cs
@@ -0,0 +1,107 @@
+﻿
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ArucoUnity
+{
+  namespace Examples
+  {
+    [Serializable]
+    public class CameraParameters
+    {
+      public DateTime CalibrationDateTime { get; set; }
+
+      public int ImageHeight { get; set; }
+
+      public int ImageWidth { get; set; }
+
+      public int CalibrationFlags { get; set; }
+
+      public float AspectRatio { get; set; }
+
+      public double[][] CameraMatrix { get; set; }
+
+      public double[][] DistCoeffs { get; set; }
+
+      public double ReprojectionError { get; set; }
+
+      public CameraParameters()
+      {
+      }
+
+      public CameraParameters(Utility.Mat cameraMatrix, Utility.Mat distCoeffs)
+      {
+        CalibrationDateTime = DateTime.Now;
+        ImportArrays(cameraMatrix, distCoeffs);
+      }
+
+      public void ImportArrays(Utility.Mat cameraMatrix, Utility.Mat distCoeffs)
+      {
+        int cameraMatrixRows = cameraMatrix.rows,
+            cameraMatrixCols = cameraMatrix.cols;
+
+        CameraMatrix = new double[cameraMatrixRows][];
+        for (int i = 0; i < cameraMatrixRows; ++i)
+        {
+          CameraMatrix[i] = new double[cameraMatrixCols];
+          for (int j = 0; j < cameraMatrixCols; ++j)
+          {
+            CameraMatrix[i][j] = cameraMatrix.AtDouble(i, j);
+          }
+        }
+
+        int distCoeffsRows = distCoeffs.rows,
+            distCoeffsCols = distCoeffs.cols;
+
+        DistCoeffs = new double[distCoeffsRows][];
+        for (int i = 0; i < distCoeffsRows; ++i)
+        {
+          DistCoeffs[i] = new double[distCoeffsCols];
+          for (int j = 0; j < distCoeffsCols; ++j)
+          {
+            DistCoeffs[i][j] = distCoeffs.AtDouble(i, j);
+          }
+        }
+      }
+
+      public void SaveToXmlFile(string filePath)
+      {
+        StreamWriter writer = null;
+        try
+        {
+          writer = new StreamWriter(filePath);
+          XmlSerializer serializer = new XmlSerializer(typeof(CameraParameters));
+          serializer.Serialize(writer, this);
+        }
+        finally
+        {
+          if (writer != null)
+          {
+            writer.Close();
+          }
+        }
+      }
+
+      public static CameraParameters LoadFromXmlFile(string filePath)
+      {
+        CameraParameters cameraParameters = null;
+        StreamReader reader = null;
+        try
+        {
+          reader = new StreamReader(filePath);
+          XmlSerializer serializer = new XmlSerializer(typeof(CameraParameters));
+          cameraParameters = (CameraParameters)serializer.Deserialize(reader);
+        }
+        finally
+        {
+          if (reader != null)
+          {
+            reader.Close();
+          }
+        }
+        return cameraParameters;
+      }
+    }
+  }
+}
