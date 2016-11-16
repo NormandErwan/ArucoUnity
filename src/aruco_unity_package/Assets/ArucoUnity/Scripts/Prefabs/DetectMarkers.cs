a0dd5383888486f2451a85199ab1872c3c06a4c0
Erwan Normand <normand.erwan@gmail.com>
2016-11-16 16:19:46 -0500

    feat(DetectMarker.cs): if the DetectMarker.cs is in estimateMode, it will use a camera-plane set configured with the camera parameters from calibration, and where the webcam texture is displayed on the plane. If not, it will use a canvas to display the webcam texture.

--- src/aruco_unity_package/Assets/ArucoUnity/Scripts/Prefabs/DetectMarkers.cs
+++ src/aruco_unity_package/Assets/ArucoUnity/Scripts/Prefabs/DetectMarkers.cs
@@ -19,13 +19,22 @@ namespace ArucoUnity
       private float markerSideLength = 0.1f;
 
       [SerializeField]
-      private bool showRejectedCandidates;
+      private bool showDetectedMarkers = true;
+
+      [SerializeField]
+      private bool showRejectedCandidates = false;
 
       [Header("Camera configuration")]
       [SerializeField]
-      private DeviceCameraController deviceCameraController;
+      private DeviceCameraCanvasDisplay deviceCameraCanvasDisplay;
 
-      [Header("Camera parameters")]
+      [SerializeField]
+      private new Camera camera;
+
+      [SerializeField]
+      private GameObject cameraPlane;
+
+      [Header("Estimation configuration")]
       [SerializeField]
       private bool estimatePose;
 
@@ -39,72 +48,105 @@ namespace ArucoUnity
       public Dictionary Dictionary { get; set; }
       public DetectorParameters DetectorParameters { get; set; }
       public float MarkerSideLength { get { return markerSideLength; } set { markerSideLength = value; } }
+      public bool ShowDetectedMarkers { get { return showDetectedMarkers; } set { showDetectedMarkers = value; } }
       public bool ShowRejectedCandidates { get { return showRejectedCandidates; } set { showRejectedCandidates = value; } }
 
       // Camera configuration
       public Texture2D ImageTexture { get; set; }
+      public Camera Camera { get { return camera; } set { camera = value; } }
+      public GameObject CameraPlane { get { return cameraPlane; } set { cameraPlane = value; } }
 
-      // Camera parameters
+      // Estimation configuration
       public bool EstimatePose { get { return estimatePose; } set { estimatePose = value; } }
       public Utility.Mat CameraMatrix { get; set; }
       public Utility.Mat DistCoeffs { get; set; }
       public GameObject DetectedMarkersObject { get { return detectedMarkersObject; } set { detectedMarkersObject = value; } }
 
       private Dictionary<int, GameObject> markerObjects;
+      private bool configurated;
+      private DeviceCameraController deviceCameraController;
 
       void OnEnable()
       {
-        DeviceCameraController.OnCameraStarted += Configurate;
+        deviceCameraController = DeviceCameraController.Instance;
+
+        configurated = false;
+        deviceCameraController.OnCameraStarted += Configurate;
       }
 
       void OnDisable()
       {
-        DeviceCameraController.OnCameraStarted -= Configurate;
+        deviceCameraController.OnCameraStarted -= Configurate;
       }
 
       void LateUpdate()
       {
-        if (deviceCameraController.cameraStarted)
+        if (deviceCameraController.cameraStarted && configurated)
         {
           Utility.Mat image;
           Utility.VectorVectorPoint2f corners;
           Utility.VectorInt ids;
           Utility.VectorVectorPoint2f rejectedImgPoints;
           Utility.VectorVec3d rvecs, tvecs;
-
-          ImageTexture.SetPixels32(deviceCameraController.activeCameraTexture.GetPixels32());
+          
           Detect(out image, out corners, out ids, out rejectedImgPoints, out rvecs, out tvecs);
         }
       }
 
-      void Configurate()
+      private void Configurate()
       {
         DetectorParameters = detectorParametersManager.detectorParameters;
         Dictionary = Methods.GetPredefinedDictionary(dictionaryName);
+        ImageTexture = deviceCameraController.activeCameraTexture2D;
 
-        ConfigurateImageTexture(deviceCameraController);
-
+        // Configurate the camera-plane group or the canvas
+        bool configurateCameraPlaneSuccess = false;
         if (estimatePose)
         {
-          ConfigurateCameraParameters(cameraParametersFilePath);
+          configurateCameraPlaneSuccess = ConfigurateCameraPlane(cameraParametersFilePath);
         }
+
+        // Activate the camera-plane group or the canvas
+        bool useCanvas = !estimatePose || !configurateCameraPlaneSuccess;
+        deviceCameraCanvasDisplay.gameObject.SetActive(useCanvas);
+        cameraPlane.gameObject.SetActive(!useCanvas);
+
+        configurated = true;
       }
 
-      public void ConfigurateImageTexture(DeviceCameraController deviceCameraController)
+      public bool ConfigurateCameraPlane(string cameraParametersFilePath)
       {
-        ImageTexture = new Texture2D(deviceCameraController.activeCameraTexture.width, deviceCameraController.activeCameraTexture.height,
-          TextureFormat.RGB24, false);
-        deviceCameraController.SetActiveTexture(ImageTexture);
-      }
-
-      public void ConfigurateCameraParameters(string cameraParametersFilePath)
-      {
+        // Camera parameters
         CameraParameters cameraParameters = CameraParameters.LoadFromXmlFile(cameraParametersFilePath);
+        if (cameraParameters == null)
+        {
+          return false;
+        }
 
         Utility.Mat cameraMatrix, distCoeffs;
         cameraParameters.ExportArrays(out cameraMatrix, out distCoeffs);
         CameraMatrix = cameraMatrix;
         DistCoeffs = distCoeffs;
+
+        // Camera configuration
+        float cameraFy = (float)cameraMatrix.AtDouble(1, 1);
+        float vFov = 2f * Mathf.Atan(0.5f * ImageTexture.height / cameraFy) * Mathf.Rad2Deg;
+        camera.fieldOfView = vFov;
+        camera.aspect = ImageTexture.width / (float)ImageTexture.height;
+        camera.transform.position = Vector3.zero;
+        camera.transform.rotation = Quaternion.identity;
+        camera.farClipPlane = cameraFy;
+
+        // Plane configuration
+        float cameraPlaneDistance = 0.5f * ImageTexture.height / Mathf.Tan(0.5f * vFov * Mathf.Deg2Rad);
+        print(cameraPlaneDistance + " " + cameraFy);
+
+        cameraPlane.transform.position = new Vector3(0, 0, cameraFy);
+        cameraPlane.transform.rotation = Quaternion.identity;
+        cameraPlane.transform.localScale = new Vector3(ImageTexture.width, ImageTexture.height, 1);
+        cameraPlane.GetComponent<Renderer>().material.mainTexture = ImageTexture;
+
+        return true;
       }
 
       public void Detect(out Utility.Mat image, out Utility.VectorVectorPoint2f corners, out Utility.VectorInt ids, 
@@ -136,7 +178,10 @@ namespace ArucoUnity
 
         if (ids.Size() > 0)
         {
-          Methods.DrawDetectedMarkers(image, corners, ids);
+          if (showDetectedMarkers)
+          {
+            Methods.DrawDetectedMarkers(image, corners, ids);
+          }
 
           if (estimatePose && DetectedMarkersObject)
           {
@@ -155,7 +200,7 @@ namespace ArucoUnity
         ImageTexture.Apply(false);
       }
 
-      void DeactivateMarkerObjects()
+      private void DeactivateMarkerObjects()
       {
         if (markerObjects != null)
         {
@@ -166,7 +211,7 @@ namespace ArucoUnity
         }
       }
 
-      void DisplayMarkerObjects(Utility.VectorInt ids, Utility.VectorVec3d rvecs, Utility.VectorVec3d tvecs)
+      private void DisplayMarkerObjects(Utility.VectorInt ids, Utility.VectorVec3d rvecs, Utility.VectorVec3d tvecs)
       {
         if (markerObjects == null)
         {
@@ -184,10 +229,11 @@ namespace ArucoUnity
             markerObjects.Add(ids.At(i), markerObject);
           }
 
+          // TODO: shift the position withe the optical center values from the camera parameters
           markerObject.transform.position = tvecs.At(i).ToPosition();
           markerObject.transform.rotation = rvecs.At(i).ToRotation();
           markerObject.SetActive(true);
-          print(i + ": " + rvecs.At(i).ToRotation() + " " + tvecs.At(i).ToPosition());
+          print(i + ": " + rvecs.At(i).ToRotation().eulerAngles + " " + tvecs.At(i).ToPosition());
         }
       }
     }
