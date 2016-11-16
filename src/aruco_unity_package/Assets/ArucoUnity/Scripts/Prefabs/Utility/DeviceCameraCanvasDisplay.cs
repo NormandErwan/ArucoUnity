9bde61e7238910de848ef24c9b437d128f16724a
Erwan Normand <normand.erwan@gmail.com>
2016-11-16 16:14:55 -0500

    refacto(DeviceCameraController.cs): extract the canvas-related code of DeviceCameraController.cs to script DeviceCameraCanvasDisplay.cs. Made DeviceCameraController.cs into a Singleton.

---
+++ src/aruco_unity_package/Assets/ArucoUnity/Scripts/Prefabs/Utility/DeviceCameraCanvasDisplay.cs
@@ -0,0 +1,85 @@
+﻿//
+// Based on: http://answers.unity3d.com/answers/1155328/view.html
+//
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ArucoUnity
+{
+  namespace Examples
+  {
+    public class DeviceCameraCanvasDisplay : MonoBehaviour
+    {
+      [SerializeField]
+      private RawImage image;
+
+      [SerializeField]
+      private RectTransform imageParent;
+
+      [SerializeField]
+      private AspectRatioFitter imageFitter;
+
+      private DeviceCameraController deviceCameraController;
+
+      // Image rotation
+      Vector3 rotationVector = new Vector3(0f, 0f, 0f);
+
+      // Image uvRect
+      Rect defaultRect = new Rect(0f, 0f, 1f, 1f);
+      Rect fixedRect = new Rect(0f, 1f, 1f, -1f);
+
+      // Image parent's scale
+      Vector3 defaultScale = new Vector3(1f, 1f, 1f);
+      Vector3 fixedScale = new Vector3(-1f, 1f, 1f);
+
+      void Awake()
+      {
+        deviceCameraController = DeviceCameraController.Instance;
+      }
+
+      void OnEnable()
+      {
+        deviceCameraController.OnActiveCameraChanged += DeviceCameraController_OnActiveCameraChanged;
+        deviceCameraController.OnCameraStarted += DeviceCameraController_OnCameraStarted;
+      }
+
+      void OnDisable()
+      {
+        deviceCameraController.OnActiveCameraChanged -= DeviceCameraController_OnActiveCameraChanged;
+        deviceCameraController.OnCameraStarted -= DeviceCameraController_OnCameraStarted;
+      }
+
+      // Set the image texture of the active camera
+      private void DeviceCameraController_OnActiveCameraChanged()
+      {
+        SetActiveTexture(deviceCameraController.activeCameraTexture2D);
+      }
+
+      // If the texture is modified outside the class, use this new texture to display
+      public void SetActiveTexture(Texture textureToUse)
+      {
+        image.texture = textureToUse;
+        image.material.mainTexture = textureToUse;
+      }
+
+      // Configure the image display when the camera is started
+      private void DeviceCameraController_OnCameraStarted()
+      {
+        // Rotate image to show correct orientation 
+        rotationVector.z = -deviceCameraController.activeCameraTexture.videoRotationAngle;
+        image.rectTransform.localEulerAngles = rotationVector;
+
+        // Set AspectRatioFitter's ratio
+        float videoRatio = deviceCameraController.activeCameraTexture.width / (float)deviceCameraController.activeCameraTexture.height;
+        imageFitter.aspectRatio = videoRatio;
+
+        // Unflip if vertically flipped
+        image.uvRect = deviceCameraController.activeCameraTexture.videoVerticallyMirrored ? fixedRect : defaultRect;
+
+        // Mirror front-facing camera's image horizontally to look more natural
+        imageParent.localScale = deviceCameraController.activeCameraDevice.isFrontFacing ? fixedScale : defaultScale;
+      }
+    }
+  }
+}
---
+++ src/aruco_unity_package/Assets/ArucoUnity/Scripts/Prefabs/Utility/DeviceCameraController.cs
@@ -0,0 +1,110 @@
+﻿//
+// Based on: http://answers.unity3d.com/answers/1155328/view.html
+//
+
+using UnityEngine;
+using System.Linq;
+using System;
+
+namespace ArucoUnity
+{
+  namespace Examples
+  {
+    public class DeviceCameraController : Singleton<DeviceCameraController>
+    {
+      // Active camera info
+      [NonSerialized]
+      public bool cameraStarted;
+
+      [NonSerialized]
+      public WebCamTexture activeCameraTexture;
+
+      [NonSerialized]
+      public Texture2D activeCameraTexture2D;
+
+      // Events
+      public delegate void CameraAction();
+      public event CameraAction OnCameraStarted;
+      public event CameraAction OnActiveCameraChanged;
+      public event CameraAction OnActiveTextureChanged;
+
+      // Device cameras
+      public WebCamDevice activeCameraDevice;
+      private WebCamDevice frontCameraDevice;
+      private WebCamDevice backCameraDevice;
+
+      void Start()
+      {
+        cameraStarted = false;
+
+        // Check for device cameras
+        if (WebCamTexture.devices.Length == 0)
+        {
+          Debug.Log("No devices cameras found");
+          return;
+        }
+
+        // Get the device's cameras
+        frontCameraDevice = WebCamTexture.devices.Last();
+        backCameraDevice = WebCamTexture.devices.First();
+
+        // Set the camera to use by default
+        SetActiveCamera(frontCameraDevice);
+      }
+
+      public void SetActiveCamera(WebCamDevice cameraToUse)
+      {
+        // Switch the activeCameraTexture
+        if (activeCameraTexture != null)
+        {
+          activeCameraTexture.Stop();
+        }
+
+        activeCameraDevice = cameraToUse;
+        activeCameraTexture = new WebCamTexture(cameraToUse.name);
+        activeCameraTexture.filterMode = FilterMode.Trilinear;
+
+        activeCameraTexture.Play();
+
+        // Reset the Texture2D
+        activeCameraTexture2D = new Texture2D(activeCameraTexture.width, activeCameraTexture.height,
+          TextureFormat.RGB24, false);
+
+        // Call the event
+        if (OnActiveCameraChanged != null)
+        {
+          OnActiveCameraChanged();
+        }
+      }
+
+      // Switch between the device's front and back camera
+      public void SwitchCamera()
+      {
+        SetActiveCamera(activeCameraDevice.Equals(frontCameraDevice) ? backCameraDevice : frontCameraDevice);
+      }
+
+      // Make adjustments to image every frame to be safe, since Unity isn't 
+      // guaranteed to report correct data as soon as device camera is started
+      void Update()
+      {
+        // Skip making adjustment for incorrect camera data
+        if (activeCameraTexture.width < 100)
+        {
+          Debug.Log("Still waiting another frame for correct info...");
+          return;
+        }
+        else
+        {
+          if (OnCameraStarted != null && !cameraStarted)
+          {
+            OnCameraStarted();
+          }
+          cameraStarted = true;
+        }
+
+        // Update the Texture2D content
+        activeCameraTexture2D.SetPixels32(activeCameraTexture.GetPixels32());
+      }
+    }
+  }
+}
