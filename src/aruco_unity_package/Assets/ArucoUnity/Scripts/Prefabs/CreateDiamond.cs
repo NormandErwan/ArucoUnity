c7636d2d4296ff5458d61dc8abe96dfd4629662b
Erwan Normand <normand.erwan@gmail.com>
2016-11-21 16:49:23 -0500

    refacto(CreateDiamond.cs, CreateDiamond.prefab): replaced public fields by properties, DiamondPlane is facing the camera, use PlaneTexture material on the DiamondPlane.

--- src/aruco_unity_package/Assets/ArucoUnity/Scripts/Prefabs/CreateDiamond.cs
+++ src/aruco_unity_package/Assets/ArucoUnity/Scripts/Prefabs/CreateDiamond.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using ArucoUnity.Utility;
+using System.IO;
 using UnityEngine;
 
 namespace ArucoUnity
@@ -7,13 +8,6 @@ namespace ArucoUnity
   {
     public class CreateDiamond : MonoBehaviour
     {
-      public Dictionary dictionary;
-
-      public Utility.Mat image;
-
-      [HideInInspector]
-      public Texture2D imageTexture;
-
       [Header("ChArUco marker configuration")]
       [SerializeField]
       [Tooltip("Marker side length (in pixels)")]
@@ -40,73 +34,82 @@ namespace ArucoUnity
 
       [Header("Draw the marker")]
       [SerializeField]
-      private bool drawMarker;
+      private bool drawDiamond;
 
       [SerializeField]
-      private GameObject markerPlane;
+      private GameObject diamondPlane;
 
       [Header("Save the marker")]
       [SerializeField]
       [Tooltip("Save the generated image")]
-      private bool saveMarker;
+      private bool saveDiamond;
 
       [SerializeField]
       [Tooltip("Output image")]
       private string outputImage = "ArucoUnity/diamond-marker.png";
 
+      // Configuration properties
+      public Dictionary Dictionary { get; set; }
+      public int SquareSideLength { get; set; }
+      public int MarkerSideLength { get; set; }
+      public int[] Ids { get; set; }
+      public int MarginsSize { get; set; }
+      public int MarkerBorderBits { get; set; }
+
+      // Diamond properties
+      public Mat Image { get; private set; }
+      public Texture2D ImageTexture { get; private set; }
+
       void Start()
       {
-        dictionary = Methods.GetPredefinedDictionary(dictionaryName);
+        Dictionary = Methods.GetPredefinedDictionary(dictionaryName);
+        SquareSideLength = squareSideLength;
+        MarkerSideLength = markerSideLength;
+        Ids = ids;
+        MarginsSize = marginsSize;
+        MarkerBorderBits = markerBorderBits;
+
         Create();
 
-        if (drawMarker)
+        if (drawDiamond)
         {
-          Draw(markerPlane);
+          Draw(diamondPlane);
 
-          if (saveMarker && outputImage.Length > 0)
+          if (saveDiamond && outputImage.Length > 0)
           {
             Save(outputImage);
           }
         }
       }
 
-      // Call it first if you're using the Script alone, not with the Prefab.
-      public void Configurate(int squareSideLength, int markerSideLength, Dictionary dictionary, int[] ids, int marginsSize, int markerBorderBits)
-      {
-        this.squareSideLength = squareSideLength;
-        this.markerSideLength = markerSideLength;
-        this.dictionary = dictionary;
-        this.ids = ids;
-        this.marginsSize = marginsSize;
-        this.markerBorderBits = markerBorderBits;
-      }
-
       public void Create()
       {
-        Utility.Vec4i ids_vec4i = new Utility.Vec4i();
-        for (int i = 0; i < ids.Length; ++i)
+        Vec4i ids_vec4i = new Vec4i();
+        for (int i = 0; i < Ids.Length; ++i)
         {
           ids_vec4i.Set(i, ids[i]);
         }
 
-        Methods.DrawCharucoDiamond(dictionary, ids_vec4i, squareSideLength, markerSideLength, out image, marginsSize, markerBorderBits);
+        Mat image;
+        Methods.DrawCharucoDiamond(Dictionary, ids_vec4i, SquareSideLength, MarkerSideLength, out image, MarginsSize, MarkerBorderBits);
+        Image = image;
 
-        imageTexture = new Texture2D(image.cols, image.rows, TextureFormat.RGB24, false);
+        ImageTexture = new Texture2D(Image.cols, Image.rows, TextureFormat.RGB24, false);
       }
 
       public void Draw(GameObject boardPlane)
       {
-        int boardDataSize = (int)(image.ElemSize() * image.Total());
-        imageTexture.LoadRawTextureData(image.data, boardDataSize);
-        imageTexture.Apply();
+        int boardDataSize = (int)(Image.ElemSize() * Image.Total());
+        ImageTexture.LoadRawTextureData(Image.data, boardDataSize);
+        ImageTexture.Apply();
 
-        boardPlane.GetComponent<Renderer>().material.mainTexture = imageTexture;
+        boardPlane.GetComponent<Renderer>().material.mainTexture = ImageTexture;
       }
 
       public void Save(string outputImage)
       {
         string imageFilePath = Path.Combine(Application.dataPath, outputImage); // TODO: use Application.persistentDataPath for iOS
-        File.WriteAllBytes(imageFilePath, imageTexture.EncodeToPNG());
+        File.WriteAllBytes(imageFilePath, ImageTexture.EncodeToPNG());
       }
     }
   }
