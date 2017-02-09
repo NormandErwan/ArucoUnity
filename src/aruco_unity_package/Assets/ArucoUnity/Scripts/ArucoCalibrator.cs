9ed0b64a6343866e552a8d380fafc11b09d4acdb
Erwan Normand <normand.erwan@protonmail.com>
2017-02-09 14:34:12 -0500

    Renamed CalibrateCameraBoard.cs to ArucoCalibrator.cs; deleted the Scripts/CalibrateCamera/ folder.

--- src/aruco_unity_package/Assets/ArucoUnity/Scripts/ArucoCalibrator.cs
+++ src/aruco_unity_package/Assets/ArucoUnity/Scripts/ArucoCalibrator.cs
@@ -11,7 +11,7 @@ namespace ArucoUnity
   /// \{
 
   // TODO: doc
-  public class CalibrateCameraBoard : ArucoObjectDetector
+  public class ArucoCalibrator : ArucoObjectDetector
   {
     // Editor fields
 
