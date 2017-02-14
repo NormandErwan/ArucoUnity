3e7f737123247de1fd7022cffab423ad7bf7a1fc
Erwan Normand <normand.erwan@protonmail.com>
2017-02-14 17:55:40 -0500

    feat(ArucoCalibratorCanvasDisplay.cs): display the image, correct the buttons, set up the canvas on the scene, doc.

--- src/aruco_unity_package/Assets/ArucoUnity/Scripts/Utility/ArucoCalibratorCanvasDisplay.cs
+++ src/aruco_unity_package/Assets/ArucoUnity/Scripts/Utility/ArucoCalibratorCanvasDisplay.cs
@@ -14,6 +14,9 @@ namespace ArucoUnity
       private ArucoCalibrator arucoCalibrator;
 
       [SerializeField]
+      private RectTransform arucoCameraImagesRect;
+
+      [SerializeField]
       private Button addFrameButton;
 
       [SerializeField]
@@ -23,7 +26,7 @@ namespace ArucoUnity
       private Button resetButton;
 
       [SerializeField]
-      private Text imagesForCalibrationText;
+      private Text framesForCalibrationText;
 
       [SerializeField]
       private Text calibrationReprojectionErrorText;
@@ -31,56 +34,115 @@ namespace ArucoUnity
       // MonoBehaviour methods
 
       /// <summary>
-      /// Set up the UI. 
+      /// Configure the UI.
       /// </summary>
       protected void Awake()
       {
+        // Configure the buttons
+        addFrameButton.enabled = true;
+        calibrateButton.enabled = false;
+        resetButton.enabled = false;
+
         addFrameButton.onClick.AddListener(AddFrameForCalibration);
         calibrateButton.onClick.AddListener(Calibrate);
         resetButton.onClick.AddListener(ResetCalibration);
+
+        // Configure the text
+        UpdateFramesForCalibrationText();
+        UpdateCalibrationReprojectionErrorText();
       }
 
+      /// <summary>
+      /// Subscribe to ArucoCalibrator configured to set the image display.
+      /// </summary>
+      protected void Start()
+      {
+        arucoCalibrator.Configured += ConfigureImagesDisplay;
+        if (arucoCalibrator.IsConfigured)
+        {
+          ConfigureImagesDisplay();
+        }
+      }
+
+      /// <summary>
+      /// Set up the images display.
+      /// </summary>
+      protected void ConfigureImagesDisplay()
+      {
+        ArucoCamera arucoCamera = arucoCalibrator.ArucoCamera;
+
+        for (int i = 0; i < arucoCamera.CamerasNumber; i++)
+        {
+          GameObject imageDisplay = new GameObject("Image " + i + " display", typeof(RectTransform));
+          RectTransform imageDisplayRect = imageDisplay.GetComponent<RectTransform>();
+          imageDisplayRect.SetParent(arucoCameraImagesRect);
+          imageDisplayRect.anchorMin = Vector2.zero;
+          imageDisplayRect.anchorMax = Vector2.one;
+          imageDisplayRect.offsetMin = imageDisplayRect.offsetMax = Vector2.zero;
+          imageDisplayRect.localScale = arucoCalibrator.ArucoCamera.ImageScalesFrontFacing[i];
+
+          RawImage imageDisplayImage = imageDisplay.AddComponent<RawImage>();
+          imageDisplayImage.texture = arucoCalibrator.ArucoCamera.ImageTextures[i];
+          imageDisplayImage.uvRect = arucoCalibrator.ArucoCamera.ImageUvRectFlips[i];
+        }
+      }
+
+      /// <summary>
+      /// Add the images of the next frame for the calibration, and update the UI.
+      /// </summary>
       private void AddFrameForCalibration()
       {
-        if (arucoCalibrator.IsConfigured || arucoCalibrator.IsCalibrated)
+        if (!arucoCalibrator.IsConfigured)
         {
           return;
         }
 
         arucoCalibrator.AddFrameForCalibration();
 
-        UpdateImagesForCalibrationText();
+        calibrateButton.enabled = true;
+        resetButton.enabled = true;
+        UpdateFramesForCalibrationText();
       }
 
+      /// <summary>
+      /// Calibrate and update the UI.
+      /// </summary>
       private void Calibrate()
       {
-        if (arucoCalibrator.IsConfigured)
+        if (!arucoCalibrator.IsConfigured)
         {
           return;
         }
 
         arucoCalibrator.Calibrate();
-
-        addFrameButton.enabled = false;
-        calibrateButton.enabled = false;
         UpdateCalibrationReprojectionErrorText();
       }
 
+      /// <summary>
+      /// Reset the calibration and update the UI.
+      /// </summary>
       private void ResetCalibration()
       {
         arucoCalibrator.ResetCalibration();
 
-        addFrameButton.enabled = true;
         calibrateButton.enabled = false;
-        UpdateImagesForCalibrationText();
+        resetButton.enabled = false;
+        UpdateFramesForCalibrationText();
         UpdateCalibrationReprojectionErrorText();
       }
 
-      void UpdateImagesForCalibrationText()
+      /// <summary>
+      /// Update the text of the number of images added for calibration.
+      /// </summary>
+      void UpdateFramesForCalibrationText()
       {
-        imagesForCalibrationText.text = "Images for calibration: " + arucoCalibrator.AllIds.Size();
+        string frames = (arucoCalibrator.AllIds != null) ? "" + arucoCalibrator.AllIds.Size() : "0";
+        framesForCalibrationText.text = "Frames for calibration: " + frames;
       }
 
+      /// <summary>
+      /// Update text for of the calibration result.
+      /// </summary>
       private void UpdateCalibrationReprojectionErrorText()
       {
         calibrationReprojectionErrorText.text = "Calibration reprojection error: "
