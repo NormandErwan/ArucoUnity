df5d7fe16078b948eeacf3550add1f41113bbeac
Erwan Normand <normand.erwan@protonmail.com>
2017-11-02 18:17:20 -0400

    Fixed the ArucoCameraWebcam.cs background size and position: webcam video live background is now correctly aligned with the 3D virtual content in the Unity scene.

--- src/aruco_unity_package/Assets/ArucoUnity/Scripts/Cameras/ArucoCameraWebcam.cs
+++ src/aruco_unity_package/Assets/ArucoUnity/Scripts/Cameras/ArucoCameraWebcam.cs
@@ -15,6 +15,10 @@ namespace ArucoUnity
     [RequireComponent(typeof(Camera))]
     public class ArucoCameraWebcam : ArucoCamera
     {
+      // Constants
+
+      protected const float cameraBackgroundDistance = 1f;
+
       // Editor fields
 
       [SerializeField]
@@ -44,13 +48,7 @@ namespace ArucoUnity
       /// <summary>
       /// <see cref="ArucoCamera.ImageRatios"/>
       /// </summary>
-      public override float[] ImageRatios
-      {
-        get
-        {
-          return new float[] { WebCamTexture.width / (float)WebCamTexture.height };
-        }
-      }
+      public override float[] ImageRatios { get { return new float[] { WebCamTexture.width / (float)WebCamTexture.height }; } }
 
       // Properties
 
@@ -79,15 +77,10 @@ namespace ArucoUnity
       /// </summary>
       public WebCamTexture WebCamTexture { get; protected set; }
 
-      /// <summary>
-      /// Camera that shot the <see cref="ArucoCamera.ImageCameras"/> in order to maintain the aspect ratio of 
-      /// <see cref="ArucoCamera.ImageTextures"/> on screen.
-      /// </summary>
-      public Camera CameraBackground { get; protected set; }
-
       // Variables
 
-      protected GameObject cameraPlane;
+      protected GameObject cameraBackground;
+      protected Renderer cameraBackgroundRenderer;
       protected bool startInitiated = false;
       protected int cameraId = 0;
 
@@ -109,7 +102,7 @@ namespace ArucoUnity
           {
             // Configure
             ImageTextures[cameraId] = new Texture2D(WebCamTexture.width, WebCamTexture.height, TextureFormat.RGB24, false);
-            ConfigureImageCameraBackgrounds();
+            ConfigureCameraAndBackground();
 
             // Update state
             startInitiated = false;
@@ -197,67 +190,55 @@ namespace ArucoUnity
       // Methods
 
       /// <summary>
-      /// Configure the <see cref="ArucoCamera.ImageCameras"/>, the <see cref="CameraBackground"/> and a facing plane of the CameraImage that will 
-      /// display the <see cref="ArucoCamera.ImageTextures"/>.
+      /// Configure the <see cref="ArucoCamera.ImageCameras"/>, the background that will display live webcam video through the
+      /// <see cref="ArucoCamera.ImageTextures"/>.
       /// </summary>
+      /// <remarks>See https://docs.opencv.org/3.3.0/d4/d94/tutorial_camera_calibration.html and 
+      /// https://docs.opencv.org/3.3.0/d9/d0c/group__calib3d.html#details for reference.</remarks>
       // TODO: handle case of CameraParameters.ImageHeight != ImageTexture.height or CameraParameters.ImageWidth != ImageTexture.width
       // TODO: handle case of CameraParameters.FixAspectRatio != 0
-      protected void ConfigureImageCameraBackgrounds()
+      protected void ConfigureCameraAndBackground()
       {
-        float CameraPlaneDistance = ImageTextures[cameraId].width;
+        float imageWidth = CameraParameters.ImageWidths[cameraId];
+        float imageHeight = CameraParameters.ImageHeights[cameraId];
+        Vector2 f = CameraParameters.GetCameraFocalLengths(cameraId);
+        Vector2 c = CameraParameters.GetCameraPrincipalPoint(cameraId);
 
-        // Configure the CameraImage according to the camera parameters
-        float farClipPlaneNewValueFactor = 1.01f; // To be sure that the camera plane is visible by the camera
-        float vFov = 2f * Mathf.Atan(0.5f * ImageTextures[cameraId].height / CameraPlaneDistance) * Mathf.Rad2Deg;
-        ImageCameras[cameraId] = GetComponent<Camera>();
-        ImageCameras[cameraId].orthographic = false;
-        ImageCameras[cameraId].fieldOfView = vFov;
-        ImageCameras[cameraId].farClipPlane = CameraPlaneDistance * farClipPlaneNewValueFactor;
+        // Configure the camera according to the camera parameters
+        if (ImageCameras[cameraId] == null)
+        {
+          ImageCameras[cameraId] = GetComponent<Camera>();
+        }
+
+        // Estimate fov using these equations : https://stackoverflow.com/questions/39992968/how-to-calculate-field-of-view-of-the-camera-from-camera-intrinsic-matrix
+        float fovX = 2f * Mathf.Atan(0.5f * imageWidth / f.x) * Mathf.Rad2Deg;
+        float fovY = 2f * Mathf.Atan(0.5f * imageHeight / f.y) * Mathf.Rad2Deg;
+        ImageCameras[cameraId].fieldOfView = fovY;
         ImageCameras[cameraId].aspect = ImageRatios[cameraId];
-        ImageCameras[cameraId].transform.position = Vector3.zero;
-        ImageCameras[cameraId].transform.rotation = Quaternion.identity;
 
-        // Configure the plane facing the CameraImage that display the texture
-        if (cameraPlane == null)
+        // Configure the background plane facing the camera
+        if (cameraBackground == null)
         {
-          cameraPlane = GameObject.CreatePrimitive(PrimitiveType.Quad);
-          cameraPlane.name = "CameraImagePlane";
-          cameraPlane.transform.parent = this.transform;
-          cameraPlane.GetComponent<Renderer>().material = Resources.Load("CameraImage") as Material;
+          cameraBackground = GameObject.CreatePrimitive(PrimitiveType.Quad);
+          cameraBackground.name = "CameraBackground";
+          cameraBackground.transform.parent = this.transform;
+          cameraBackground.transform.rotation = Quaternion.identity;
+
+          cameraBackgroundRenderer = cameraBackground.GetComponent<Renderer>();
+          cameraBackgroundRenderer.material = Resources.Load("CameraBackground") as Material;
         }
+        cameraBackgroundRenderer.material.mainTexture = ImageTextures[cameraId];
 
-        cameraPlane.transform.position = new Vector3(0, 0, CameraPlaneDistance);
-        cameraPlane.transform.rotation = Quaternion.identity;
-        cameraPlane.transform.localScale = new Vector3(ImageTextures[cameraId].width, ImageTextures[cameraId].height, 1);
-        cameraPlane.GetComponent<Renderer>().material.mainTexture = ImageTextures[cameraId];
-        cameraPlane.SetActive(DisplayImages);
+        // Estimate background plane position relative to the camera, considering the first link in remarks: here x=0.5*ImageWidth and y=0.5*ImageHeight
+        // (center of the camera projection), w=Z=cameraBackgroundDistance and we are looking for X=posX and Y=posY
+        float posX = (0.5f * imageWidth - c.x) / f.x * cameraBackgroundDistance;
+        float posY = (0.5f * imageHeight - c.y) / f.y * cameraBackgroundDistance;
+        cameraBackground.transform.position = new Vector3(posX, posY * -1, cameraBackgroundDistance); // c=-1 for posY is because OpenCV (u=0, v=0) camera coordinates origin is top-left, but bottom-left in Unity (see https://docs.unity3d.com/ScriptReference/Camera.html)
 
-        // If preserving the aspect ratio of the CameraImage, create a second camera that shot it as background
-        if (PreserveAspectRatio)
-        {
-          if (CameraBackground == null)
-          {
-            GameObject CameraBackgroundGameObject = new GameObject("BlackBackgroundCamera");
-            CameraBackgroundGameObject.transform.parent = this.transform;
-
-            CameraBackground = CameraBackgroundGameObject.AddComponent<Camera>();
-            CameraBackground.clearFlags = CameraClearFlags.SolidColor;
-            CameraBackground.backgroundColor = Color.black;
-            CameraBackground.depth = ImageCameras[cameraId].depth + 1; // Render after the CameraImage
-
-            CameraBackground.orthographic = false;
-            CameraBackground.fieldOfView = ImageCameras[cameraId].fieldOfView;
-            CameraBackground.nearClipPlane = ImageCameras[cameraId].nearClipPlane;
-            CameraBackground.farClipPlane = ImageCameras[cameraId].farClipPlane;
-            CameraBackground.transform.position = ImageCameras[cameraId].transform.position;
-            CameraBackground.transform.rotation = ImageCameras[cameraId].transform.rotation;
-          }
-          CameraBackground.gameObject.SetActive(true);
-        }
-        else if (CameraBackground != null)
-        {
-          CameraBackground.gameObject.SetActive(false);
-        }
+        // Estimate background plane scale, using the same equations used for fov but here we are looking for w=scaleX and h=scaleY
+        float scaleX = 2f * cameraBackgroundDistance * Mathf.Tan(fovX * Mathf.Deg2Rad / 2f);
+        float scaleY = 2f * cameraBackgroundDistance * Mathf.Tan(fovY * Mathf.Deg2Rad / 2f);
+        cameraBackground.transform.localScale = new Vector3(scaleX, scaleY, 1);
       }
     }
   }
