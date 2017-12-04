7ea5f8dff0e148e4ac67f95a778114eead972cae
Erwan Normand <normand.erwan@protonmail.com>
2017-12-04 15:35:17 -0500

    Added a script to export the package with the layers for the backgrounds cameras.

---
+++ src/aruco_unity_package/Assets/Editor/ExportArucoUnityPackage.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+
+public static class ExportArucoUnityPackage
+{
+  [MenuItem("Aruco Unity/Export package")]
+  public static void ExportPackage()
+  {
+    string[] projectContent = new string[]
+    {
+      "Assets/ArucoUnity/Materials",
+      "Assets/ArucoUnity/Plugins",
+      "Assets/ArucoUnity/Prefabs",
+      "Assets/ArucoUnity/Scenes",
+      "Assets/ArucoUnity/Scripts",
+      "Assets/ArucoUnity/Scenes",
+      "ProjectSettings/TagManager.asset"
+    };
+    AssetDatabase.ExportPackage(projectContent, "ArucoUnity.unitypackage", ExportPackageOptions.Interactive | ExportPackageOptions.Recurse);
+  }
+}
