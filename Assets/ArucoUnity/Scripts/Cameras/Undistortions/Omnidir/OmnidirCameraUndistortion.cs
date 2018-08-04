3b4ac99c51b9f7c1ae0318a6a7b795fc40aa0698
Erwan Normand <normand.erwan@protonmail.com>
2018-08-05 00:58:33 +0200

    Docs update

--- Assets/ArucoUnity/Scripts/Cameras/Undistortions/Omnidir/OmnidirCameraUndistortion.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ArucoUnity.Cameras.Undistortions
-{
-  /// <summary>
-  /// Manages the undistortion and rectification process for fisheye and omnidir <see cref="ArucoCamera"/>.
-  /// </summary>
-  public class OmnidirCameraUndistortion : OmnidirCameraUndistortionGeneric<ArucoCamera>
-  {
-  }
-}
